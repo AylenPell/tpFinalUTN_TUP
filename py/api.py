22b51eb0c0884e1badeda19391a0591c81308bd1
Aylen Pellegrini <aylen.pell@gmail.com>
2024-11-11 23:05:16 -0300

    manda el mail
    con el JSON acomodado

--- py/api.py
+++ py/api.py
@@ -56,11 +56,79 @@ def get_info_dolares():
                 'nombre': i['nombre'],
                 'moneda': i['moneda'],
                 'fechaActualizacion': i['fechaActualizacion']                
-            })
+            })        
         return info_moneda
     else:
         return jsonify({'error': 'Tu vieja'}), response.status_code
-   
+    
+@app.route('/print_dolares', methods=['GET'])
+def print_info_dolares(): 
+    url = "https://dolarapi.com/v1/dolares"
+    response = requests.get(url)   
+    print(response.json(), "Datos recibidos de la API")  
+    if response.status_code == 200: # OK 
+        data = response.json()
+        info_moneda = []
+        
+        for i in data:
+            info_moneda.append({
+                'casa': i['casa'],
+                'compra': i['compra'],
+                'venta': i['venta'],
+                'nombre': i['nombre'],
+                'moneda': i['moneda'],
+                'fechaActualizacion': i['fechaActualizacion']                
+            })
+        email_body = "Cotización Dólares:\n\n"
+        for item in info_moneda:
+            email_body += f"Casa: {item['casa']}\n"
+            email_body += f"Compra: {item['compra']}\n"
+            email_body += f"Venta: {item['venta']}\n"
+            email_body += f"Nombre: {item['nombre']}\n"
+            email_body += f"Moneda: {item['moneda']}\n"
+            email_body += f"Fecha de Actualización: {item['fechaActualizacion']}\n"
+            email_body += "-" * 30 + "\n"  # Separador entre cada casa de cambio
+        
+        return email_body
+    else:
+        return jsonify({'error': 'Tu vieja'}), response.status_code
+
+@app.route('/print_generales', methods=['GET'])
+def print_info_general(): 
+    url = "https://dolarapi.com/v1/cotizaciones"
+    response = requests.get(url)   
+    print(response.json(), "Datos recibidos de la API")  
+    if response.status_code == 200: # OK 
+        data = response.json()
+        info_moneda = []
+        
+        for i in data:
+            info_moneda.append({
+                'casa': i['casa'],
+                'compra': i['compra'],
+                'venta': i['venta'],
+                'nombre': i['nombre'],
+                'moneda': i['moneda'],
+                'fechaActualizacion': i['fechaActualizacion']                
+            })
+        email_body = "Cotizaciones generales:\n\n"
+        for item in info_moneda:
+            email_body += f"Casa: {item['casa']}\n"
+            email_body += f"Compra: {item['compra']}\n"
+            email_body += f"Venta: {item['venta']}\n"
+            email_body += f"Nombre: {item['nombre']}\n"
+            email_body += f"Moneda: {item['moneda']}\n"
+            email_body += f"Fecha de Actualización: {item['fechaActualizacion']}\n"
+            email_body += "-" * 30 + "\n"  # Separador entre cada casa de cambio
+        
+        return email_body
+    else:
+        return jsonify({'error': 'Tu vieja'}), response.status_code
+
+body_content_dolares = print_info_dolares()
+body_content_general = print_info_general()
+body_content = body_content_dolares + body_content_general
+
 @app.route('/historico', methods=['GET'])
 def get_info_historico(): 
     url = "https://api.argentinadatos.com/v1/cotizaciones/dolares/"
@@ -84,55 +152,51 @@ def get_info_historico():
 # envio mail
 
 
-# @app.route('/procesar', methods=['POST'])
-# def procesar():
-#     nombre = request.form.get('nombre')
-#     correo = request.form.get('correo')
-             
-#     if nombre and correo:
-#         print(f"Nombre: {nombre}")
-#         print(f"Correo: {correo}")
-#         data = {
-#             'service_id': 'Sr_Cambio',
-#             'template_id': 'cotizaciones',
-#             'user_id': 'ceqgeO6X6IYONKkDy',
-#             'accessToken': 'CD-KRNfbyAP1_CaGEoUzQ',
-#             'template_params': {
-#                 'user_email': correo,
-#                 'from_name': 'Sr Cambio',
-#                 'user_name': nombre,
-#                 'message': json.dumps(get_info_dolares())
-#             }
-#         }
-#         headers = {
-#             'Content-Type': 'application/json',
-#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-#             'Accept': 'application/json, text/javascript, */*; q=0.01',
-#             'Accept-Language': 'en-US,en;q=0.9',
-#             'Origin': 'http://127.0.0.1:5000/',  
-#             'Referer': 'http://127.0.0.1:5000/'
-#         }
+@app.route('/procesar', methods=['POST'])
+def procesar():
+    nombre = request.form.get('nombre')
+    correo = request.form.get('correo')
 
-#         try:
-#             response = requests.post(
-#                 'https://api.emailjs.com/api/v1.0/email/send',
-#                 data=json.dumps(data),
-#                 headers=headers
-#             )
-#             response.raise_for_status()
-#             print('La cotización fue enviada correctamente!')
-#         except requests.exceptions.RequestException as error:
-#             print(f'Oops... {error}')
-#             if error.response is not None:
-#                 print(error.response.text)
+    if nombre and correo:
+        print(f"Nombre: {nombre}")
+        print(f"Correo: {correo}")
+        data = {
+            'service_id': 'Sr_Cambio',
+            'template_id': 'cotizaciones',
+            'user_id': 'ceqgeO6X6IYONKkDy',
+            'accessToken': 'CD-KRNfbyAP1_CaGEoUzQ',
+            'template_params': {
+                'user_email': correo,
+                'from_name': 'Sr Cambio',
+                'user_name': nombre,
+                'message': body_content
+            }
+        }
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Origin': 'http://127.0.0.1:5000/',  
+            'Referer': 'http://127.0.0.1:5000/'
+        }
+
+        try:
+            response = requests.post(
+                'https://api.emailjs.com/api/v1.0/email/send',
+                data=json.dumps(data),
+                headers=headers
+            )
+            response.raise_for_status()
+            print('La cotización fue enviada correctamente!')
+        except requests.exceptions.RequestException as error:
+            print(f'Oops... {error}')
+            if error.response is not None:
+                print(error.response.text)
     
-#         return jsonify({
-#             'mensaje': f"Datos recibidos correctamente",
-#             'nombre': nombre,
-#             'correo': correo
-#         }), 200     
-#     else:
-#         return jsonify({'error': "Amigue, completá bien los datos"}), 405
+        return f'Mensaje envidado correctamente a {correo}', 200     
+    else:
+        return jsonify({'error': "Amigue, completá bien los datos"}), 405
     
 
     
