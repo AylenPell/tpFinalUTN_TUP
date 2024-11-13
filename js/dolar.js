ac5c8414efb7d33f27d4ae7ce03e46a1c7240232
Aylen Pellegrini <aylen.pell@gmail.com>
2024-11-13 19:44:01 -0300

    correccion de comparacion horaria
    cambiamos 5 min por 24hrs

--- js/dolar.js
+++ js/dolar.js
@@ -3,40 +3,40 @@
 
 // Llamada api de Python
 fetch(`http://127.0.0.1:5000/dolares`)
-  .then(response => response.json())
-  .then(data => {for (i=0; i<data.length; i++){
-      agregarCotizacion(data[i].nombre, data[i].compra.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 }), data[i].venta.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 }), data[i].fecha)
-      document.getElementsByClassName("card")[i+1].style.display="grid"
-      console.log(data)
-      }
-    });
+.then(response => response.json())
+.then(data => {for (i=0; i<data.length; i++){
+    agregarCotizacion(data[i].nombre, data[i].compra.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 }), data[i].venta.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 }), data[i].fecha)
+    document.getElementsByClassName("card")[i+1].style.display="grid"
+    console.log(data)
+    }
+});
 
-    function agregarCotizacion(nombre, compra, venta, fecha){
-      var x = document.getElementsByClassName("card")[0].cloneNode(true)
-      x.querySelector(".titulo").innerHTML= nombre
-      x.querySelector(".precioVenta").innerHTML= venta
-      x.querySelector(".precioCompra").innerHTML= compra
-  
-      console.log(fecha);
-      let fechaConvertida;
-      if (fecha) {
-          // Intentar convertir el string de fecha a un objeto Date
-          fechaConvertida = new Date(fecha);
-          if (isNaN(fechaConvertida.getTime())) {
-              console.error("Fecha inválida:", fecha);
-              fechaConvertida = null;
-          }
-      }
-      console.log(fechaConvertida);
-      const cincoMinutos = 5 * 60 * 1000; 
-      const diferenciaTiempo = fechaConvertida ? (new Date() - fechaConvertida) : Infinity;
-      console.log(diferenciaTiempo);
-  
-      if (diferenciaTiempo >= cincoMinutos) {
-          x.querySelectorAll(".imgOk")[0].src = '../img/dedo_not.png';
-      } else {
-          x.querySelectorAll(".imgOk")[0].src = '../img/dedo_ok.png';
-      }
-  
-      document.body.querySelector('.bodyCards').appendChild(x) 
-  }
+function agregarCotizacion(nombre, compra, venta, fecha){
+    var x = document.getElementsByClassName("card")[0].cloneNode(true)
+    x.querySelector(".titulo").innerHTML= nombre
+    x.querySelector(".precioVenta").innerHTML= venta
+    x.querySelector(".precioCompra").innerHTML= compra
+
+    console.log(fecha);
+    let fechaConvertida;
+    if (fecha) {
+        // Intentar convertir el string de fecha a un objeto Date
+        fechaConvertida = new Date(fecha);
+        if (isNaN(fechaConvertida.getTime())) {
+            console.error("Fecha inválida:", fecha);
+            fechaConvertida = null;
+        }
+    }
+    console.log(fechaConvertida);
+    const veinticuatroHoras = 24 * 60 * 60 * 1000;
+    const diferenciaTiempo = fechaConvertida ? (new Date() - fechaConvertida) : Infinity;
+    console.log(diferenciaTiempo);
+
+    if (diferenciaTiempo >= veinticuatroHoras) {
+        x.querySelectorAll(".imgOk")[0].src = '../img/dedo_not.png';
+    } else {
+        x.querySelectorAll(".imgOk")[0].src = '../img/dedo_ok.png';
+    }
+
+    document.body.querySelector('.bodyCards').appendChild(x) 
+}
